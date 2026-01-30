79949a258106dd21b1641378a87b26545ce1cd2d
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2026-01-30 10:40:22 +0530

    AND Primitive

---
+++ Problem Statement - 29/AND Primitive.v
@@ -0,0 +1,9 @@
+module and2_prim (
+    input  a,
+    input  b,
+    output y
+);
+    // TODO: instantiate the built-in AND gate primitive
+    // and <instance_name> ( <out>, <in1>, <in2> );
+    and a1(y,a,b);
+endmodule
